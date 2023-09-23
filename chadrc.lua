694316c2b5b42c75175fe38157e8e066af263418
Tore Pettersen <toreskog@live.com>
2023-09-23 23:28:16 +0200

    Setup custom snippets

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,9 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'onedark' }
+vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
+
+M.ui = { theme = "onedark" }
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
 
---
+++ snippets/elixir.lua
@@ -0,0 +1,8 @@
+-- Example snippets
+-- https://github.com/L3MON4D3/LuaSnip/blob/master/Examples/snippets.lua
+
+-- local ls = require "luasnip"
+-- local s = ls.snippet
+-- local t = ls.text_node
+
+return {}
